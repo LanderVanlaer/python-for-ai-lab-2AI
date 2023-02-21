faf3ab4f8291fe985e10a3e73beda67e2d69a409
Lander Van laer <lander.vanlaer1@gmail.com>
2023-02-21 17:23:52 +0100

    1.9

---
+++ src/01/09.py
@@ -0,0 +1,6 @@
+# Create a lambda expression that reverses the string passed to it and print the result.
+#
+# Example:
+#   nohtyP (when the string was ‘Python’)
+
+print((lambda s: s[::-1])("Python"))
